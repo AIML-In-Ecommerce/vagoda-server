c5f19ba3f6cc8f222d8dd42da5cab0e769fc2fb7
MiwaUS7605 <minhquang9802@gmail.com>
2024-05-23 18:15:10 +0700

    feat: implement payment service

---
+++ Payment/index.js
@@ -0,0 +1,37 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import bodyParser from 'body-parser';
+
+import { specs, swaggerUi } from './configs/swagger.js';
+import { errorHandler, notFound } from './shared/helper/errorHandler.js';
+import db from "./configs/db.js";
+
+import paymentRoute from "./routes/payment.route.js";
+const app = express();
+
+dotenv.config();
+const port = process.env.PAYMENT_PORT;
+db();
+
+const initializeExpress = (app) => {
+  //app.use(express.static(path.join(__dirname, "public")));
+  //app.use(cors(corsOptions));
+  app.use(cors())
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  //app.use(morgan("combined", { stream: logger.stream }));
+};
+
+initializeExpress(app);
+
+app.use(paymentRoute);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+app.use(notFound);
+app.use(errorHandler);
+app.listen(port, () => {
+  console.log(`Your app is running at http://localhost:${port}`);
+});
