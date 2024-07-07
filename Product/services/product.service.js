eaa1bcb40e58c546152c1890eab95c82a1bd2672
thien <79550249+thienhk15@users.noreply.github.com>
2024-07-07 14:34:45 +0700

    keyword filter

--- Product/services/product.service.js
+++ Product/services/product.service.js
@@ -56,81 +56,101 @@ const ProductService = {
       throw new Error("Error while fetching top selling products");
     }
   },
-    getFilteredProducts: async (filterOptions) => {
-      const {
-        _id,
-        keyword,
-        shopId,
-        minPrice,
-        maxPrice,
-        category,
-        subCategory,
-        subCategoryTypes,
-        avgRating,
-        sortBy,
-        index,
-        amount,
-        brand,
-        status,
-      } = filterOptions;
-      const query = {};
-      if (typeof minPrice !== "number" || typeof maxPrice !== "number") {
-        throw new Error(
-          "Invalid price values. Please provide valid numbers for minPrice and maxPrice."
-        );
-      }
-      // Apply filters
-      if (_id !== "") query._id = _id;
-      if (keyword) query.name = { $regex: keyword, $options: "i" };
-      if (shopId) query.shop = shopId;
-      if (minPrice !== 0 || maxPrice !== Number.MAX_VALUE)
-        query.finalPrice = {
-          $gte: minPrice, // Greater than or equal to minPrice
-          $lte: maxPrice, // Less than or equal to maxPrice
-        };
+  getFilteredProducts: async (filterOptions) => {
+    const {
+      _id,
+      keyword,
+      shopId,
+      minPrice,
+      maxPrice,
+      category,
+      subCategory,
+      subCategoryTypes,
+      avgRating,
+      sortBy,
+      index,
+      amount,
+      brand,
+      status,
+    } = filterOptions;
+    const query = {};
+    if (typeof minPrice !== "number" || typeof maxPrice !== "number") {
+      throw new Error(
+        "Invalid price values. Please provide valid numbers for minPrice and maxPrice."
+      );
+    }
+    // Apply filters
+    if (_id !== "") query._id = _id;
+    // if (keyword) query.name = { $regex: keyword, $options: "i" };
+    if (keyword) {
+      const regex = { $regex: keyword, $options: "i" };
+      console.log(regex);
+      const categories = await Category.find({ name: regex }).select("_id");
+      const subCategories = await SubCategory.find({ name: regex }).select(
+        "_id"
+      );
+      const subCategoryTypes = await SubCategoryType.find({
+        name: regex,
+      }).select("_id");
+      query.$or = [
+        { name: regex },
+        { description: regex },
+        { category: { $in: categories } },
+        { subCategory: { $in: subCategories } },
+        { subCategoryType: { $in: subCategoryTypes } },
+      ];
+    }
+    if (shopId) query.shop = shopId;
+    if (minPrice !== 0 || maxPrice !== Number.MAX_VALUE)
+      query.finalPrice = {
+        $gte: minPrice, // Greater than or equal to minPrice
+        $lte: maxPrice, // Less than or equal to maxPrice
+      };
 
-      if (category.length > 0) query.category = { $in: category };
-      if (subCategory.length > 0) query.subCategory = { $in: subCategory };
-      if (subCategoryTypes.length > 0)
-        query.subCategoryTypes = { $in: subCategoryTypes };
-      if (avgRating !== null) {
-        query.avgRating = { $gte: avgRating - 1, $lte: avgRating };
-      }
-      if (brand !== "") query.brand = brand;
-      if (status !== "") query.status = status;
-      console.log(query);
+    if (category.length > 0) query.category = { $in: category };
+    if (subCategory.length > 0) query.subCategory = { $in: subCategory };
+    if (subCategoryTypes.length > 0)
+      query.subCategoryTypes = { $in: subCategoryTypes };
+    if (avgRating !== null) {
+      query.avgRating = { $gte: avgRating - 1, $lte: avgRating };
+    }
+    if (brand !== "") query.brand = brand;
+    if (status !== "") query.status = status;
+    console.log(query);
 
-      const total = await Product.countDocuments(query);
-      // Sort products
-      let sortOption = {};
-      switch (sortBy) {
-        case "ascending price":
-          sortOption = { finalPrice: 1 };
-          break;
-        case "descending price":
-          sortOption = { finalPrice: -1 };
-          break;
-        case "top sale":
-          sortOption = { soldQuantity: -1 };
-          break;
-        case "highest rating":
-          sortOption = { avgRating: -1 };
-          break;
-        case "latest":
-          sortOption = { createdAt: -1 };
-          break;
-        default:
-          break;
-      }
-      //console.log(sortOption)
-      console.log(index, amount);
-      const filteredProducts = await Product.find(query)
-        .sort(sortOption)
-        .skip((index - 1) * amount)
-        .limit(amount);
-      //console.log(filteredProducts)
-      return { filteredProducts, total };
-    },
+    const total = await Product.countDocuments(query);
+    console.log(total);
+    // Sort products
+    let sortOption = {};
+    switch (sortBy) {
+      case "ascending price":
+        sortOption = { finalPrice: 1 };
+        break;
+      case "descending price":
+        sortOption = { finalPrice: -1 };
+        break;
+      case "top sale":
+        sortOption = { soldQuantity: -1 };
+        break;
+      case "highest rating":
+        sortOption = { avgRating: -1 };
+        break;
+      case "latest":
+        sortOption = { createdAt: -1 };
+        break;
+      default:
+        break;
+    }
+    //console.log(sortOption)
+    console.log(index, amount);
+    const filteredProducts = await Product.find(query)
+      .sort(sortOption)
+      .skip((index - 1) * amount)
+      .limit(amount)
+      .exec();
+    //console.log(filteredProducts)
+    return { filteredProducts, total };
+  },
   async searchProductsByName(keyword) {
     const query = { $text: { $search: keyword } };
     const filteredProducts = await Product.find(query);
@@ -229,8 +249,8 @@ const ProductService = {
     // subCategory.subCategoryTypes = null;
 
     const products = data.map((item) => {
-      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-      console.log(item["Danh mục   *"])
+      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+      console.log(item["Danh mục   *"]);
       let ctg = item["Danh mục   *"].split("/"); // Nữ/Áo nữ/Áo thun
       console.log(ctg);
       let categories = JSON.parse(JSON.stringify(ctgs));
@@ -360,28 +380,26 @@ const ProductService = {
     return updatedProductIdList;
   },
 
-  async getAllFlashSalesProducts(amount)
-  {
+  async getAllFlashSalesProducts(amount) {
     const flashSalesProducts = await Product.find({
       $expr: {
-        $lt: ['$finalPrice', '$originalPrice']
-      }
-    })
-    if(flashSalesProducts == null)
-    {
-      return null
+        $lt: ["$finalPrice", "$originalPrice"],
+      },
+    });
+    if (flashSalesProducts == null) {
+      return null;
     }
 
-    return flashSalesProducts
+    return flashSalesProducts;
   },
 
   async getRelatedProductsBySubCategory(subCategories) {
     if (subCategories.length === 0) {
-    const topSellingProducts = await this.getTopSelling(4);
+      const topSellingProducts = await this.getTopSelling(4);
       return topSellingProducts;
     }
     const relatedProducts = [];
-    
+
     for (const subCategoryId of subCategories) {
       const products = await Product.find({ subCategory: subCategoryId })
         .sort({ soldQuantity: -1 })
