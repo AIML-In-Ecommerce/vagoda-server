8f67c43cd519d87251ea33ce8f095f5e01756783
PHoaiLe <lehoaiphuong13042002@gmail.com>
2024-07-13 10:55:43 +0700

    fix: add redis password to cache connection

--- Statistics/configs/redis.config.js
+++ Statistics/configs/redis.config.js
@@ -11,7 +11,7 @@ const redisClient = createClient(
             host: process.env.REDIS_HOST || 'localhost',
             port: process.env.REDIS_PORT || 6379
         },
-        // password: process.env.REDIS_PASSWORD
+        password: process.env.REDIS_PASSWORD
     }
 )
 
