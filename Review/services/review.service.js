46b87943b08db70c88b897aeaf1cb64a6dc8763f
thien <79550249+thienhk15@users.noreply.github.com>
2024-07-18 14:02:30 +0700

    filter review by cat

--- Review/services/review.service.js
+++ Review/services/review.service.js
@@ -30,12 +30,11 @@ const ReviewService = {
     return await Review.findByIdAndDelete(id);
   },
   async addCommentIdToReview(reviewId, commentId) {
-     return await Review.findByIdAndUpdate(
-        reviewId, 
-        { $push: { conversation: { comment: commentId } } },
-        { new: true, useFindAndModify: false }
-      );
-
+    return await Review.findByIdAndUpdate(
+      reviewId,
+      { $push: { conversation: { comment: commentId } } },
+      { new: true, useFindAndModify: false }
+    );
   },
   async averageRating(productId) {
     const reviews = await this.getByProductId(productId);
@@ -54,61 +53,67 @@ const ReviewService = {
       amount,
       sortBy,
     } = filterOptions;
-    console.log("service filter option: ", filterOptions)
-  
+    console.log("service filter option: ", filterOptions);
+
     const query = {};
-  
+
     if (shop) {
       query.shop = shop;
     }
-  
+
     // if (product) {
     //   query['product.name'] = { $regex: product, $options: 'i' };
     // }
     if (product) {
-      const filteredProduct = await Product.find({ name: { $regex: product, $options: 'i' } });
-      const listProductId = filteredProduct.map(product => product._id);
+      const filteredProduct = await Product.find({
+        name: { $regex: product, $options: "i" },
+      });
+      const listProductId = filteredProduct.map((product) => product._id);
       query.product = { $in: listProductId };
     }
 
     if (category != "") {
-      query.category = category;
+      const filteredProductByCategory = await Product.find({ category });
+      const listProductIdByCategory = filteredProductByCategory.map(
+        (product) => product._id
+      );
+      query.product = { $in: listProductIdByCategory };
     }
-  
+
     if (rating !== null && !isNaN(rating)) {
       query.rating = rating;
     }
-  
+
     if (isResponse) {
       query.isResponseByShop = isResponse;
     }
-  
+
     console.log("Query:", JSON.stringify(query, null, 2));
-  
+
     const total = await Review.countDocuments(query);
-  
+
     let sortOption = {};
     switch (sortBy) {
-      case 'ascending rating':
+      case "ascending rating":
         sortOption = { rating: 1 };
         break;
-      case 'descending rating':
+      case "descending rating":
         sortOption = { rating: -1 };
         break;
-      case 'latest':
+      case "latest":
         sortOption = { createdAt: -1 };
         break;
       default:
         break;
     }
-  
+
     console.log("Sort Option:", sortOption);
-  
+
     const filteredReviews = await Review.find(query)
       .populate({
-        path: 'product',
+        path: "product",
         populate: {
-          path: 'name attribute shop',
+          path: "name attribute shop",
         },
       })
       // .populate('user', 'username')
@@ -116,12 +121,11 @@ const ReviewService = {
       .skip((index - 1) * amount)
       .limit(amount)
       .exec();
-  
+
     // console.log("Filtered Reviews:", JSON.stringify(filteredReviews, null, 2));
-  
+
     return { filteredReviews, total };
   },
-
 };
 
 export default ReviewService;
