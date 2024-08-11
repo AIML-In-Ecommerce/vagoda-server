416911ae984a3b15a01fd22809561427f175406b
PHoaiLe <lehoaiphuong13042002@gmail.com>
2024-08-11 16:47:52 +0700

    Feat: auto update promotion status

--- Promotion/cron_job/jobs/update_promotion_status.job.js
+++ Promotion/cron_job/jobs/update_promotion_status.job.js
@@ -1,16 +1,44 @@
-
+import { CronJob } from "cron"
 
 
 class CronJob_UpdatePromotionStatus
 {
-    constructor()
+    constructor(key = null, isAutoRestarted = false, isOnceTimeJob = true)
     {
+        this.isOnceTimeJob = isOnceTimeJob
+        this.isAutoRestarted = isAutoRestarted
+        this.key = key
         this.jobs = []
     }
 
     initialize(isOnceTimeJob = undefined, key = undefined, isAutoRestarted = undefined)
     {
-        
+        const newJob = CronJob.from({
+            cronTime: "*/2 * * * * *",
+            onTick: () => {
+                console.log("promotion")
+            }
+        })
+    }
+
+    start()
+    {
+
+    }
+
+    #stop()
+    {
+
+    }
+
+    #regenerate()
+    {
+
+    }
+
+    #restart()
+    {
+
     }
 
 
